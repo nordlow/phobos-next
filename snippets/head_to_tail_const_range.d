1a3717ad272cea31bc386f0502136e94866a8898
per_nordlow <per.nordlow@gmail.com>
2021-03-23 21:38:27 +0100

    *** empty log message ***

--- snippets/head_to_tail_const_range.d
+++ snippets/head_to_tail_const_range.d
@@ -1,4 +1,8 @@
-@safe pure unittest
+@safe unittest
 {
-	import std.algorithm;
+	import std.algorithm.iteration : map;
+    import std.stdio;
+    auto x = [1, 2];
+    auto y = x.map!(_ => _*_);
+    writeln(y);
 }
