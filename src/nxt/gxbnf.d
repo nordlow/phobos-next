988b7bfa0da146b0a4e628941b96f011c7a073ff
per_nordlow <per.nordlow@gmail.com>
2021-03-28 17:06:45 +0200

    *** empty log message ***

--- src/nxt/gxbnf.d
+++ src/nxt/gxbnf.d
@@ -963,7 +963,7 @@ private:
         } ();
         const lc = offsetLineColumn(_input, offset);
         import nxt.conv_ex : toDefaulted;
-        const string tokString = token.tok.toDefaulted!string("unknown");
+        const tokString = token.tok.toDefaulted!string("unknown");
         debug printf("%.*s(%u,%u): %s: %.*s, `%.*s` (%.*s) at offset %llu\n",
                cast(int)path.length, &path[0],
                lc.line + 1, lc.column + 1,
