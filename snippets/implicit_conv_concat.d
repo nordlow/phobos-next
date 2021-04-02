29cbb15adac21c0d6d7033cd7fd4f2185c630a91
per_nordlow <per.nordlow@gmail.com>
2021-04-03 00:06:00 +0200

    *** empty log message ***

--- snippets/implicit_conv_concat.d
+++ snippets/implicit_conv_concat.d
@@ -1,8 +1,19 @@
+import std.stdio;
+
 // See_Also: https://forum.dlang.org/post/omfkmlsqamxjtktnaqic@forum.dlang.org
-@safe pure unittest
+int main(string[] args)
 {
-	const(char)[] x;
-    string y;
-    string z1 = x ~ y; // errors
-    string z2 = y ~ x; // errors
+	const(char)[] x = "hello ";
+    string y = "world";
+
+    string z1 = x ~ y; // will pass
+    assert(z1 == "hello world");
+
+    string z2 = y ~ x; // will pass
+    assert(z2 == "hello world");
+
+    writeln(z1);
+    writeln(z2);
+
+    return 0;
 }
