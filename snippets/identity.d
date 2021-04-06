5d077e937092d2fce540bd673129266c84ad2284
per_nordlow <per.nordlow@gmail.com>
2021-04-06 22:47:59 +0200

    *** empty log message ***

--- snippets/identity.d
+++ snippets/identity.d
@@ -1,3 +1,16 @@
+// See: https://forum.dlang.org/post/uhwjlhxojafhahby// yoms@forum.dlang.org
+
+auto ref T identity(T)(auto ref T arg)
+{
+    return arg;
+}
+
+auto ref T identity_fwd(T)(auto ref T arg)
+{
+    import core.lifetime : forward;
+    return forward!arg;
+}
+
 @safe pure unittest
 {
 
