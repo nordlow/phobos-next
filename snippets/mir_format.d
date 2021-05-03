367f8d9a45353cdaf04b99b89cf76aefef7cc3f7
per_nordlow <per.nordlow@gmail.com>
2021-05-03 23:02:20 +0200

    *** empty log message ***

--- snippets/mir_format.d
+++ snippets/mir_format.d
@@ -1,15 +1,13 @@
 @safe pure unittest
 {
-    import mir.conv: to;
+    import mir.conv : to;
     import mir.small_string;
     alias S = SmallString!32;
 
-// Floating-point numbers are formatted to
-// the shortest precise exponential notation.
+    // Floating-point numbers are formatted to the shortest precise exponential notation.
     assert(123.0.to!S == "123.0");
     assert(123.to!(immutable S) == "123");
     assert(true.to!S == "true");
     assert(true.to!string == "true");
-
     assert((cast(S)"str")[] == "str");
 }
