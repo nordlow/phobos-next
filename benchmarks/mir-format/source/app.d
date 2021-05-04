92b79b7921ed8574f867efc7437cd875d13a8d14
Per Nordlöw <PNordlow@symmetryinvestments.com>
2021-05-04 21:20:49 +0200

    *** empty log message ***

--- benchmarks/mir-format/source/app.d
+++ benchmarks/mir-format/source/app.d
@@ -5,9 +5,13 @@ int main(string[] args)
 {
     version(useMir)
     {
+        import mir.format : text;
+        assert(text("hello", " world ", 42) == "hello world 42");
     }
     version(usePhobos)
     {
+        import std.format :  format;
+        assert(format("%s %s %d", "hello", "world", 42) == "hello world 42");
     }
 	return 0;
 }
